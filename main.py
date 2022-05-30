d6e6900a1370c44eaaddb6bb02eb13757af582f1
senseech <maxic1234@gmail.com>
2022-05-30 21:08:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+# name = input("Введите ваше имя: ")
+# print("Привет,", name)
+#
+# if 1 < 2:
+#     print("1<2")
+# # comment
+# # comment
+# '''
+# comment
+# '''
+# print(input("привет"))
+# b = True
+# if b:
+#     print(123)
+# c = 10
+# d = 0b10
+# print(c+d)
+# x = 0.9e-30
+# print(x)
+# message = "Hello World!"
+# print(message)  # Hello World!
+# name = 'Tom'
+# print(name)  # Tom
+# text = ("Laudate omnes gentes laudate "
+#         "Magnificat in secula ")
+# print(text)
+# text = '''Laudate omnes gentes laudate
+# Magnificat in secula
+# Et anima mea laudate
+# Magnificat in secula
+# '''
+# print(text)
+# userName = "Tom"
+# userAge = 37
+# user = f"name: {userName}  age: {userAge}"
+# print(user)   # name: Tom  age: 37
+# userId = "abc"  # тип str
+# print(type(userId))  # <class 'str'>
+# userId = 234  # тип int
+# print(type(userId))  # <class 'int'>
+# g = 0b101
+# print(g)
+# print(f"{g:0b}")
+# print(f"{g:0d}")
+# f = 0b10101
+# d = 0b11010
+# a = f & d
+# print(a)
+# print(a)
+# a = 6
+# print(~a)
+# if language == "english":
+#     print("English")
+#     if daytime == "morning":
+#         print("Good morning")
+#     else:
+#         print("Good evening")
+# mes = "123qwe123"
+# for b in mes:
+#     print(b, end="")
+# i = 1
+# j = 1
+# while i < 10:
+#     while j < 10:
+#         print(i * j, end="\t")
+#         j += 1
+#     print("\n")
+#     j = 1
+#     i += 1
+# def sum(*numbers):
+#     result = 0
+#     for n in numbers:
+#         result += n
+#     print(f"sum = {result}")
+#
+#
+# sum(1, 2, 3, 4, 5)  # sum = 15
+# sum(3, 4, 5, 6)  # sum = 18
+
+
+
+
+
+
+
+
+
+
+
+
+
