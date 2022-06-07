edfee1ae8f539cbdc7d3716bbdb8a588c707e5be
senseech <maxic1234@gmail.com>
2022-06-07 15:07:08 +0300

    init1781287

--- main.py
+++ main.py
@@ -119,26 +119,127 @@
 # outer()  # 5
 # 25    - inner
 # 5     - outer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# def outer():  # внешняя функция
+#     n = 5
+#
+#     def inner():  # вложенная функция
+#         nonlocal n  # указываем, что n - это переменная из окружающей функции
+#         n = 25
+#         print(n)
+#
+#     inner()  # 25
+#     print(n)
+#
+#
+# outer()  # 25
+# def outer():  # внешняя функция
+#     n = 5  # лексическое окружение - локальная переменная
+#
+#     def inner():  # локальная функция
+#         nonlocal n
+#         n += 1  # операции с лексическим окружением
+#         print(n)
+#
+#     return inner
+#
+# tmp = outer()
+# tmp()
+# tmp()
+# tmp()
+# tmp()
+# tmp1 = outer()
+# tmp1()
+# tmp1()
+# tmp1()
+# tmp1()
+# class Person:
+#     def say(self, message):
+#         print(message)
+#
+#     def sayHello(self):
+#         self.say("Q")
+#
+#
+# asd = Person()
+# asd.say("asd")
+# asd.sayHello()
+#
+#
+# class Person:
+#
+#     def say(self, message):
+#         print(message)
+#
+#     def say_hello(self):
+#         self.say("Hello work")  # обращаемся к выше определенному методу say
+#
+#
+# tom = Person()
+# tom.say_hello()
+# class Person:
+#     def __init__(self, name):
+#         self.__name = name
+#         self.__age = 1
+#
+#     def say(self):
+#         print("yo-la")
+#         print(f"my name {self.__name} ama {self.__age} butcher")
+#
+#     def __set_name__(self, name):
+#         self.__name = name
+#
+#
+# tom = Person("tom")
+# class Person:
+#     def __init__(self):
+#         # self.__name = name
+#         # self.__age = age
+#         self.__name = None
+#         print("gogo")
+#
+#     @property
+#     def name(self):
+#         return self.__name
+#
+#     @name.setter
+#     def name(self, name):
+#         self.__name = name
+#
+#     def say(self):
+#         print(f"{self.__name}")
+#
+#
+# class Worker(Person):
+#     def say(self):
+#         super().say()
+#         super(Worker, self).say()
+#         print("qweqwe1")
+#
+#
+# tom = Person()
+# tom.name = "assdd"
+# tom.say()
+# tom.__set_name__("ann")
+# tom.say()
+# tom.name = "qweqwe"
+# print(tom.name)
+#
+# bob.name = "bob"
+# bob = Worker()
+# bob.say()
+# tom.say()
+# numbers = [1, 2, 3, 4, 5, 6, 0]
+# print(sorted(numbers))
+# print(numbers)
+# numbers = range(0, 10, 2)
+# print(numbers)
+# for i in range(1, 3):
+#     print(i, end=" ")
+numbers = {
+    1: 123,
+    2: 234,
+    3: 345,
+    4: 456
+}
+for key in numbers.values():
+    print(key)
